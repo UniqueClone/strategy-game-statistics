b111b1a346e3c7ddc9f9d8b733bb9954387dbf9c
Ryan Lynch <lynchryan2@gmail.com>
2024-02-07 21:10:04 +0000

    Adding tailwindcss.

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: true,
+    content: [
+        "./index.html",
+        "./**/*.{js,jsx,ts,tsx}"
+    ],  
+    theme: {
+    extend: {},
+    },
+    plugins: [],
+}
+
