c0b115a243d31ece06b6ade4c7aeff6566aa1d59
Ryan Lynch <lynchryan2@gmail.com>
2024-02-25 15:44:30 +0000

    Adding docs.

--- src/AddEditEntry/Inputs/MapDetails.ts
+++ src/AddEditEntry/Inputs/MapDetails.ts
@@ -1,4 +1,13 @@
+/**
+ * Represents the details of a map.
+ */
 export interface MapDetails {
+    /**
+     * The name of the map.
+     */
     map: string;
+    /**
+     * Indicates whether the map is the free map of the week.
+     */
     isFreeMapOfTheWeek: boolean;
 }
--- src/AddEditEntry/Inputs/MapInput.mappers.ts
+++ src/AddEditEntry/Inputs/MapInput.mappers.ts
@@ -1,9 +1,24 @@
 import { MapDetails } from "./MapDetails";
 
+/**
+ * Sets the map name in the map details object.
+ * 
+ * @param map - The new map name.
+ * @param mapDetails - The current map details object.
+ * @param setMapDetails - The function to update the map details object.
+ * @returns void
+ */
 export const setMapName = (map: string, mapDetails: MapDetails, setMapDetails: (value: MapDetails) => void) => {
     setMapDetails({...mapDetails, map});
 };
 
+/**
+ * Sets the value of `isFreeMapOfTheWeek` in the `mapDetails` object and updates it using the `setMapDetails` function.
+ * 
+ * @param isFreeMapOfTheWeek - The new value for `isFreeMapOfTheWeek`.
+ * @param mapDetails - The current map details object.
+ * @param setMapDetails - The function to update the map details object.
+ */
 export const setIsFreeMap = (isFreeMapOfTheWeek: boolean, mapDetails: MapDetails, setMapDetails: (value: MapDetails) => void) => {
     setMapDetails({...mapDetails, isFreeMapOfTheWeek});
 };
