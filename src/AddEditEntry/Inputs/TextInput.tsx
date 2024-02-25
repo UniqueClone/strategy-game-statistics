e45482f2becc07f245d2edfdab29d6fbfb4011c2
Ryan Lynch <lynchryan2@gmail.com>
2024-02-25 19:34:18 +0000

    Added className customisation.

--- src/AddEditEntry/Inputs/TextInput.tsx
+++ src/AddEditEntry/Inputs/TextInput.tsx
@@ -6,14 +6,14 @@ import { TextInputProps } from "./TextInputProps";
  * @returns The rendered text input element.
  */
 export const TextInput: React.FC<TextInputProps> = (props) => {
-    const { value, onChange } = props;
+    const { value, onChange, className } = props;
 
     return (
         <input
+            className={className || "border border-gray-300 px-2 py-1 rounded"}
+            onChange={(event) => onChange(event.target.value)}
             type="text"
             value={value}
-            onChange={(event) => onChange(event.target.value)}
-            className="border border-gray-300 px-2 py-1 rounded"
         />
     );
 };
--- src/AddEditEntry/Inputs/TextInputProps.ts
+++ src/AddEditEntry/Inputs/TextInputProps.ts
@@ -4,13 +4,22 @@
 export interface TextInputProps {
     /**
      * The current value of the text input.
-     */
-    value: string;
+    */
+   value: string;
+
+   /**
+    * A callback function that is called when the value of the text input changes.
+    * @param value - The new value of the text input.
+   */
+  onChange: (value: string) => void;
+
     /**
-     * A callback function that is called when the value of the text input changes.
-     * @param value - The new value of the text input.
+     * An optional class name for the text input.
+     * @default "border border-gray-300 px-2 py-1 rounded"
+     * @example "border border-red-300 px-2 py-1 rounded"
      */
-    onChange: (value: string) => void;
+    className?: string;
+
     /**
      * An optional callback function that is called when the text input loses focus.
      * @param value - The current value of the text input.
